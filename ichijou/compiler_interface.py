c268e9346850e062745f0ad7dc250ac6c76a7c93
Jonathan Rainer <jr776@york.ac.uk>
2019-02-22 10:38:06 +0000

    Added file to generate boot program

--- ichijou/compiler_interface.py
+++ ichijou/compiler_interface.py
@@ -1,6 +1,11 @@
+from pathlib import Path
+from jinja2 import Template
+
+
 class CompilerInterface(object):
 
     riscv_binary_prefix = ""
+    boot_file_template = Path("~", "Documents", "PhD", "Ichijou", "templates", "boot.template").expanduser()
 
     def __init__(self, riscv_binary_prefix):
         self.riscv_binary_prefix = riscv_binary_prefix
@@ -8,6 +13,19 @@ class CompilerInterface(object):
     def create_linker_file(self):
         return
 
-    def create_boot_program(self):
-        return
+    def create_boot_program(self, temporary_path, stack_pointer_location):
+        return self.create_file_from_template(
+            self.boot_file_template, temporary_path, "boot.S",
+            {"stack_pointer_loc": stack_pointer_location})
+
+    @staticmethod
+    def create_file_from_template(template_file_path, temporary_path, output_file_name, params):
+        with open(str(template_file_path)) as boot_file_pointer:
+            template = Template(boot_file_pointer.read())
+        output_path = Path(temporary_path, output_file_name)
+        with open(str(output_path), "w") as output_file_pointer:
+            output_file_pointer.write(template.render(**params))
+        return output_path
+
+
 
--- ichijou/main.py
+++ ichijou/main.py
@@ -2,12 +2,18 @@ import sys
 
 from pathlib import Path
 
+from ichijou.compiler_interface import CompilerInterface
+
 
 class Ichijou(object):
 
-    def run_experiment(self, benchmark_path):
+    compiler_interface = CompilerInterface(
+        Path("/opt/riscv/bin")
+    )
+
+    def run_experiment(self, benchmark_path, temporary_file_location):
         # Compile the benchmark
-        self.compile_benchmark(benchmark_path)
+        self.compile_benchmark(benchmark_path, temporary_file_location)
         # Generate the MEM File for the benchmark
         # Open up Vivado with the correct design // Open the VLAB
         # Generate the Bitstream if it doesn't exist
@@ -17,13 +23,17 @@ class Ichijou(object):
         # Process the output into graphs etc.
         return 0
 
-    def compile_benchmark(self, benchmark_path):
-        # Create a Boot File & Linker Script
+    def compile_benchmark(self, benchmark_path, temporary_file_location):
+        # Create a Boot File
+        boot_file_location = self.compiler_interface.create_boot_program(temporary_file_location, "ff00")
+        # Create Linker Script
+        linker_file_location = self.compiler_interface.create_linker_file()
         # Run compile option
+        # Clean up the temporary files
         # Return the address of the ELF file generated
         return
 
 
 if __name__ == "__main__":
     system = Ichijou()
-    system.run_experiment(Path(sys.argv[1]))
+    system.run_experiment(Path(sys.argv[1]), sys.argv[2])
