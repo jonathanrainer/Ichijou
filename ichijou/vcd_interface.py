ad936aa01ec650ad04019f3acc3b5e7db1ae140a
Jonathan Rainer <jr776@york.ac.uk>
2019-06-28 17:05:08 +0100

    Discovered way to extract the necessary metrics, needs finessing a bit and abstracting into easier to use functions for repetitive behaviour.

--- ichijou/vcd_interface.py
+++ ichijou/vcd_interface.py
@@ -1,12 +1,26 @@
 from pathlib import Path
+from Verilog_VCD.Verilog_VCD import parse_vcd
 
 
 class VCDInterface(object):
 
     @staticmethod
     def does_raw_result_exist(benchmark, experiment_type, experiment_directory):
-        result_file = Path(experiment_directory, "output", "{0}_{1}_ila_results.vcd".format(benchmark, experiment_type))
+        result_file = Path(experiment_directory, "results", "{0}_{1}_ila_results.vcd".format(benchmark, experiment_type))
         return result_file.exists()
 
     def extract_results(self, benchmark, experiment_type, experiment_directory, trigger_values):
-        return "Hello World"
+        vcd_file = parse_vcd(Path(experiment_directory, "results",
+                                  "{}_{}_ila_results.vcd".format(benchmark, experiment_type)))
+        addr_values = \
+            [x['tv'] for x in vcd_file.values() if x['nets'][0]["name"] == "k_top/system_ila_0/inst/probe1_1[15:0]"][0]
+        addr_time = [x for x in addr_values if int(x[1], base=2) == int("254", base=16)][0][0]
+        gnt_values = \
+            [x['tv'] for x in vcd_file.values() if x['nets'][0]["name"] == "k_top/system_ila_0/inst/probe4_1"][0]
+        gnt_time = [y for y in gnt_values if y[0] > addr_time][0][0]
+        req_values = \
+            [x['tv'] for x in vcd_file.values() if x['nets'][0]["name"] == "k_top/system_ila_0/inst/probe2_1"][0]
+        start_of_req = [x for x in req_values if x[0] < gnt_time][-1][0]
+        counter_values = \
+            [x['tv'] for x in vcd_file.values() if x['nets'][0]["name"] == "k_top/system_ila_0/inst/probe3_1[31:0]"][0]
+        return [int(y[1], base=16) for y in counter_values if y[0] == start_of_req]
